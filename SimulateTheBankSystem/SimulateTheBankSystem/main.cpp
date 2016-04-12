f78f86d937a2e82ef671ee0d9a558c462649636e
Traim304 <Traim304@163.com>
2016-04-12 19:52:30 +0800

    created

---
+++ SimulateTheBankSystem/SimulateTheBankSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimulateTheBankSystem
+//
+//  Created by JacobLan on 16/4/12.
+//  Copyright © 2016年 JacobLan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
