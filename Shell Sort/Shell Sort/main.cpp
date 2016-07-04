6158f96bbf2200adf51218db6820a873b8e94d95
Traim304 <Traim304@163.com>
2016-07-04 14:21:55 +0800

    Shell Sort

---
+++ Shell Sort/Shell Sort/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace  std;
+
+int main(int argc, const char * argv[]) {
+    cout << "将对几个数据进行直接插入排序?" << endl;
+    int size;
+    cin >> size;
+    int* array = new int[size];
+    for (int i = 0 ; i < size; i++) {
+        cout << "请输入第 " << i+1 << "个数字:";
+        cin >> array[i];
+    }
+    
+    //开始排序
+    int d = size/2;         //增量 d
+    while (d) {
+        //对相距 d 的元素,使用插入排序
+        //i <= size-1
+        for (int i = d; i <= size-d; i = i + d) {
+            int tmp = array[i];
+            int j = 0;
+            for (j = i-d; j >= 0 && tmp < array[j]; j = j - d) {
+                array[j+d] = array[j];
+            }
+            array[j+d] = tmp;
+        }
+        d = d/2;
+    }
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << ' ';
+    }
+
+    return 0;
+}
